0489bfa0ce9d70e8528960fdba6d2d70aae45a6b
James Merkin <jnmerk@gmail.com>
2019-03-30 14:29:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function removeElementFromBeginningOfArray(array){
   return(newarray);
 }
 
-function destructivelyRemoveElementFromEndofArray (array){
+function destructivelyRemoveElementFromEndOfArray (array){
 array.pop();
 return(array);
 }
