7f1f6f2c91606b741650d3f680c7bc78db145fa2
James Merkin <jnmerk@gmail.com>
2019-03-30 14:07:49 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ['snickers','hundred grand', 'kitkat','skittles'];
+
+function addElementToBeginningOfArray(array,element){
+var newarray=[];
+newarray=[element,...array];
+return(newarray);
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray (array,element){
+  newarray=[]; 
+  newarray=[...array,element];
+  return newarray;
+  }
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push (element);
+ return(array);
+}
+
+function accessElementInArray (array, index){
+var element;
+element=array[index]; 
+return (element);
+}
+  
